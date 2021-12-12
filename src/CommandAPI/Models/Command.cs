c8fc8ddc89e35cf2bdfbd37eaf5c48f8c2ff4590
Ogbonna James C <jamogbosoft@gmail.com>
2021-12-12 21:17:22 +0100

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandAPI.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Platform { get; set; }
+        public string CommandLine { get; set; }
+    }
+}
