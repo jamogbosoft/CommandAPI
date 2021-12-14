ec31abd0801680a525611031f2d4dc680819999a
Ogbonna James C <jamogbosoft@gmail.com>
2021-12-14 17:53:10 +0100

    Connected to an SQL Server Database

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,25 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
-            
+            services.AddDbContext<CommandContext>(opt =>
+            {
+                opt.UseSqlServer(
+                    Configuration.GetConnectionString("API_Conn"));
+            });
+
+            _ = services.AddControllers(); 
+            // _ = services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            _ = services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,15 +36,15 @@ namespace CommandAPI
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                _ = app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            _ = app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-               endpoints.MapControllers();
-            });
+            _ = app.UseEndpoints(endpoints =>
+              {
+                  _ = endpoints.MapControllers();
+              });
         }
     }
 }
